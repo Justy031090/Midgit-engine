ae87fc8ebe84311a650459cb1a38fb69e870eb7f
justy031090 <justy031090@gmail.com>
2022-01-02 18:54:49 +0200

    reducer fix

--- src/components/context/AppReducer.js
+++ src/components/context/AppReducer.js
@@ -1,4 +1,4 @@
-export default (state, action) => {
+const AppReducer = (state, action) => {
     switch (action.type) {
         case 'ADD_REPO_TO_WATCHLIST':
             return {
@@ -16,3 +16,4 @@ export default (state, action) => {
             return state;
     }
 };
+export default AppReducer;
